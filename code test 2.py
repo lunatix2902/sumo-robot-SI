ce7c62c8fc36a7f59f8be2000ccbede8094eb10b
Lunatix <thismonokuma@gmail.com>
2025-01-23 08:21:55 +0100

    Update code test 2.py

--- code test 2.py
+++ code test 2.py
@@ -1,78 +1,88 @@
-
 from microbit import *
 from machine import time_pulse_us
 import utime
 
-# Line Finder on pin12
-# Line Finder on pin8
-# Ultrasonic on pin0
+# Line Finder sur pin12 et pin8
+# Capteur ultrasonique (trigger et echo) sur pin0
 
+# Fonction pour obtenir la distance mesurée par le capteur ultrasonique
 def getUltrasonicData(trig, echo, data='distance', timeout_us=30000):
-  trig.write_digital(0)
-  utime.sleep_us(2)
-  trig.write_digital(1)
-  utime.sleep_us(10)
-  trig.write_digital(0)
-  echo.read_digital()
-  duration = time_pulse_us(echo, 1, timeout_us)/1e6 # t_echo in seconds
-  if duration > 0:
-    if data == 'distance':
-      #sound speed, round-trip/2, get in cm
-      return 343 * duration/2 * 100
-    elif data == 'duration':
-      return duration
+    trig.write_digital(0)
+    utime.sleep_us(2)
+    trig.write_digital(1)
+    utime.sleep_us(10)
+    trig.write_digital(0)
+    echo.read_digital()
+    duration = time_pulse_us(echo, 1, timeout_us) / 1e6  # Temps en secondes
+    if duration > 0:
+        if data == 'distance':
+            # Calculer la distance en cm
+            return 343 * duration / 2 * 100
+        elif data == 'duration':
+            return duration
+        else:
+            raise ValueError("Data option '" + data + "' is not valid")
     else:
-      raise ValueError("Data option '" + data + "' is not valid")
-  else:
-    return -1
+        return -1
 
+# Fonctions pour contrôler les moteurs gauche et droit
 def codo_controlLeftMotor(cmd, speed):
-  pin13.write_analog(cmd[0] * speed)
-  pin14.write_analog(cmd[1] * speed)
+    pin13.write_analog(cmd[0] * speed)
+    pin14.write_analog(cmd[1] * speed)
 
 def codo_controlRightMotor(cmd, speed):
-  pin15.write_analog(cmd[0] * speed)
-  pin16.write_analog(cmd[1] * speed)
+    pin15.write_analog(cmd[0] * speed)
+    pin16.write_analog(cmd[1] * speed)
 
-def codo_move(direction, speed = 1023):
-  if (speed >= 0 and speed <= 1023):
-    if direction == 'forward':
-      codo_controlLeftMotor([0, 1], speed)
-      codo_controlRightMotor([0, 1], speed)
-    elif direction == 'backward':
-      codo_controlLeftMotor([1, 0], speed)
-      codo_controlRightMotor([1, 0], speed)
-    elif direction == 'right':
-      codo_controlLeftMotor([0, 1], speed)
-      codo_controlRightMotor([1, 0], speed)
-    elif direction == 'left':
-      codo_controlLeftMotor([1, 0], speed)
-      codo_controlRightMotor([0, 1], speed)
-    elif direction == 'stop':
-      codo_controlLeftMotor([0, 0], speed)
-      codo_controlRightMotor([0, 0], speed)
+# Fonction pour contrôler le mouvement du robot
+def codo_move(direction, speed=1023):
+    if 0 <= speed <= 1023:
+        if direction == 'forward':
+            codo_controlLeftMotor([0, 1], speed)
+            codo_controlRightMotor([0, 1], speed)
+        elif direction == 'backward':
+            codo_controlLeftMotor([1, 0], speed)
+            codo_controlRightMotor([1, 0], speed)
+        elif direction == 'right':
+            codo_controlLeftMotor([0, 1], speed)
+            codo_controlRightMotor([1, 0], speed)
+        elif direction == 'left':
+            codo_controlLeftMotor([1, 0], speed)
+            codo_controlRightMotor([0, 1], speed)
+        elif direction == 'stop':
+            codo_controlLeftMotor([0, 0], speed)
+            codo_controlRightMotor([0, 0], speed)
+        else:
+            display.scroll("'" + str(direction) + "' is not a direction")
     else:
-      display.scroll("'" + str(direction) + "' is not a direction")
-  else:
-    raise ValueError('The speed of codo motors have to be set between 0 and 255')
+        raise ValueError('The speed of codo motors must be set between 0 and 1023')
 
+# Boucle principale
 while True:
-  while pin12.read_digital() == True and pin8.read_digital() == True:
-    codo_move('right', 1000)
-    codo_move('stop')
-    if getUltrasonicData(pin0, pin0, 'distance') < 5:
-      codo_move('right', 400)
-      codo_move('stop')
-      utime.sleep(1)
-      codo_move('forward', 1023)
-      utime.sleep(1)
-      codo_move('right', 650)
-      utime.sleep(0.75)
-      codo_move('forward', 1023)
-      codo_move('stop')
-      codo_move('right', 800)
-      codo_move('stop')
-    else:
-      codo_move('stop')
-      codo_move('right')
-
+    # Vérifier l'état des capteurs de ligne
+    while pin12.read_digital() == True and pin8.read_digital() == True:
+        codo_move('stop')  # Stop en attendant un adversaire
+        
+        # Vérifier la distance avec le capteur ultrasonique
+        distance = getUltrasonicData(pin0, pin0, 'distance')
+        
+        if distance > 0 and distance < 10:  # Si un adversaire est détecté à moins de 10 cm
+            # Avancer pour attaquer
+            codo_move('forward', 1023)
+            utime.sleep(1)  # Durée de l'attaque
+            codo_move('stop')
+            
+            # Reculer pour battre en retraite
+            codo_move('backward', 800)
+            utime.sleep(1)  # Durée de la retraite
+            codo_move('stop')
+            
+            # Se repositionner en tournant
+            codo_move('right', 800)
+            utime.sleep(0.75)  # Ajustez la durée pour un bon angle
+            codo_move('stop')
+        else:
+            # Si aucun adversaire détecté, faire un mouvement de recherche
+            codo_move('right', 600)
+            utime.sleep(0.5)
+            codo_move('stop')
